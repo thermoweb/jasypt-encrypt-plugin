5b03f31ab3d2968fcb1a0d1442fb569c3c21a68b
Romain CHAUVEAU <40058692+thermoweb@users.noreply.github.com>
2023-07-05 16:50:08 +0200

    Feature/issue 1 remember options (#2)
    * clean code
    
    * add feature : remember last option selection (by project)
    
    * make "encapsulate" checked by default
    add unreleased version in changelog

--- src/main/java/org/thermoweb/intellij/plugin/encrypt/EncryptStringAction.java
+++ src/main/java/org/thermoweb/intellij/plugin/encrypt/EncryptStringAction.java
@@ -4,7 +4,6 @@ import java.util.Map;
 
 import org.jetbrains.annotations.NotNull;
 
-import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.command.WriteCommandAction;
@@ -13,40 +12,23 @@ import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
 
-import static org.thermoweb.intellij.plugin.encrypt.CipherInformationsDialog.ALGORITHM_FIELD_NAME;
-import static org.thermoweb.intellij.plugin.encrypt.CipherInformationsDialog.ENCAPSULATE_FIELD_NAME;
-import static org.thermoweb.intellij.plugin.encrypt.CipherInformationsDialog.PASSWORD_FIELD_NAME;
+import static org.thermoweb.intellij.plugin.encrypt.CipherInformationsDialog.*;
 
-public class EncryptStringAction extends AnAction {
+public class EncryptStringAction extends JasyptAction {
 	@Override
 	public void actionPerformed(@NotNull final AnActionEvent event) {
-		CipherInformationsDialog dialog = new CipherInformationsDialog();
+		super.actionPerformed(event);
 		if (!dialog.showAndGet()) {
 			return;
 		}
 
 		Map<String, String> values = dialog.getValues();
 
-		Editor editor = event.getRequiredData(CommonDataKeys.EDITOR);
-		Project project = event.getRequiredData(CommonDataKeys.PROJECT);
-		Document document = editor.getDocument();
-		Caret primaryCaret = editor.getCaretModel().getPrimaryCaret();
 		String newValue = CipherUtils.encrypt(primaryCaret.getSelectedText(), values.get(PASSWORD_FIELD_NAME), values.get(ALGORITHM_FIELD_NAME));
 		final String cipheredPassword = "true".equals(values.get(ENCAPSULATE_FIELD_NAME)) ? "ENC(" + newValue + ")" : newValue;
 		WriteCommandAction.runWriteCommandAction(project,
 				() -> document.replaceString(primaryCaret.getSelectionStart(), primaryCaret.getSelectionEnd(), cipheredPassword));
-
+		updateSettings();
 		primaryCaret.removeSelection();
-	}
-
-	@Override
-	public void update(@NotNull final AnActionEvent event) {
-		// Get required data keys
-		Project project = event.getProject();
-		Editor editor = event.getData(CommonDataKeys.EDITOR);
-
-		// Set visibility only in the case of
-		// existing project editor, and selection
-		event.getPresentation().setEnabledAndVisible(project != null && editor != null && editor.getSelectionModel().hasSelection());
 	}
 }
--- src/main/java/org/thermoweb/intellij/plugin/encrypt/UncryptStringAction.java
+++ src/main/java/org/thermoweb/intellij/plugin/encrypt/UncryptStringAction.java
@@ -1,64 +1,38 @@
 package org.thermoweb.intellij.plugin.encrypt;
 
 import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.Optional;
 
 import org.jetbrains.annotations.NotNull;
 
-import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.command.WriteCommandAction;
-import com.intellij.openapi.editor.Caret;
-import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
 
 import static org.thermoweb.intellij.plugin.encrypt.CipherInformationsDialog.ALGORITHM_FIELD_NAME;
-import static org.thermoweb.intellij.plugin.encrypt.CipherInformationsDialog.ENCAPSULATE_FIELD_NAME;
 import static org.thermoweb.intellij.plugin.encrypt.CipherInformationsDialog.PASSWORD_FIELD_NAME;
 
-public class UncryptStringAction extends AnAction {
-	private final Pattern pattern = Pattern.compile("ENC\\((.*)\\)");
+public class UncryptStringAction extends JasyptAction {
 
-	@Override
-	public void actionPerformed(@NotNull final AnActionEvent event) {
-		CipherInformationsDialog dialog = new CipherInformationsDialog();
-		if (!dialog.showAndGet()) {
-			return;
-		}
+    @Override
+    public void actionPerformed(@NotNull final AnActionEvent event) {
+        super.actionPerformed(event);
+        if (!dialog.showAndGet()) {
+            return;
+        }
 
-		Map<String, String> values = dialog.getValues();
+        Map<String, String> values = dialog.getValues();
+        final Optional<String> textToUncrypt = getSelectedText();
+        if (textToUncrypt.isEmpty()) {
+            return;
+        }
 
-		Editor editor = event.getRequiredData(CommonDataKeys.EDITOR);
-		Project project = event.getRequiredData(CommonDataKeys.PROJECT);
-		Document document = editor.getDocument();
-		Caret primaryCaret = editor.getCaretModel().getPrimaryCaret();
-		if (primaryCaret.getSelectedText() == null) {
-			return;
-		}
-
-		Matcher matches = pattern.matcher(primaryCaret.getSelectedText());
-		final String selectedText = matches.find() && "true".equals(values.get(ENCAPSULATE_FIELD_NAME)) ?
-				matches.group(1) :
-				primaryCaret.getSelectedText();
-
-		WriteCommandAction.runWriteCommandAction(project,
-				() -> document.replaceString(primaryCaret.getSelectionStart(), primaryCaret.getSelectionEnd(),
-						CipherUtils.decrypt(selectedText, values.get(PASSWORD_FIELD_NAME), values.get(ALGORITHM_FIELD_NAME))));
-
-		primaryCaret.removeSelection();
-	}
-
-	@Override
-	public void update(@NotNull final AnActionEvent event) {
-		// Get required data keys
-		Project project = event.getProject();
-		Editor editor = event.getData(CommonDataKeys.EDITOR);
-
-		// Set visibility only in the case of
-		// existing project editor, and selection
-		event.getPresentation().setEnabledAndVisible(project != null && editor != null && editor.getSelectionModel().hasSelection());
-	}
+        WriteCommandAction.runWriteCommandAction(project,
+                () -> document.replaceString(primaryCaret.getSelectionStart(), primaryCaret.getSelectionEnd(),
+                        CipherUtils.decrypt(textToUncrypt.get(), values.get(PASSWORD_FIELD_NAME), values.get(ALGORITHM_FIELD_NAME))));
+        updateSettings();
+        primaryCaret.removeSelection();
+    }
 }
