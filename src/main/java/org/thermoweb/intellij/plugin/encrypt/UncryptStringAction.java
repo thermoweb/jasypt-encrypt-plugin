0a4e478f2e7c4b647a443696e3dbbdc5bb843ab7
Romain CHAUVEAU <40058692+thermoweb@users.noreply.github.com>
2023-07-13 14:04:34 +0200

    unused imports (#4)

--- src/main/java/org/thermoweb/intellij/plugin/encrypt/UncryptStringAction.java
+++ src/main/java/org/thermoweb/intellij/plugin/encrypt/UncryptStringAction.java
@@ -6,10 +6,7 @@ import java.util.Optional;
 import org.jetbrains.annotations.NotNull;
 
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.command.WriteCommandAction;
-import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.project.Project;
 
 import static org.thermoweb.intellij.plugin.encrypt.CipherInformationsDialog.ALGORITHM_FIELD_NAME;
 import static org.thermoweb.intellij.plugin.encrypt.CipherInformationsDialog.PASSWORD_FIELD_NAME;
