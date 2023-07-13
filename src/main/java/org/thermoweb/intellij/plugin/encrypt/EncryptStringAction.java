8d32e8e5bbd88b404a1763d11de4ee649a44ecb2
Romain Chauveau <romain.chauveau@praxedo.com>
2023-07-13 13:43:40 +0200

    unused imports

--- src/main/java/org/thermoweb/intellij/plugin/encrypt/EncryptStringAction.java
+++ src/main/java/org/thermoweb/intellij/plugin/encrypt/EncryptStringAction.java
@@ -5,12 +5,7 @@ import java.util.Map;
 import org.jetbrains.annotations.NotNull;
 
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.command.WriteCommandAction;
-import com.intellij.openapi.editor.Caret;
-import com.intellij.openapi.editor.Document;
-import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.project.Project;
 
 import static org.thermoweb.intellij.plugin.encrypt.CipherInformationsDialog.*;
 
