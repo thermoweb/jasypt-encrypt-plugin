7061701e99d635c85934e73e37e0503b0f23ba1c
romain.chauveau <romain.chauveau@praxedo.com>
2024-10-08 09:48:31 +0200

    clean

--- src/main/java/org/thermoweb/intellij/plugin/encrypt/cipher/CipherUtils.java
+++ src/main/java/org/thermoweb/intellij/plugin/encrypt/cipher/CipherUtils.java
@@ -8,29 +8,28 @@ import org.jasypt.iv.RandomIvGenerator;
 import org.thermoweb.intellij.plugin.encrypt.exceptions.JasyptPluginException;
 
 public class CipherUtils {
-	private CipherUtils() {
+    private CipherUtils() {
+    }
 
-	}
+    public static String encrypt(final String value, final String password, final String algorithm) {
+        return stringEncryptor(password, algorithm).encrypt(value);
+    }
 
-	public static String encrypt(final String value, final String password, final String algorithm) {
-		return stringEncryptor(password, algorithm).encrypt(value);
-	}
+    public static String decrypt(final String value, final String password, final String algorithm) throws JasyptPluginException {
+        try {
+            return stringEncryptor(password, algorithm).decrypt(value);
+        } catch (EncryptionOperationNotPossibleException e) {
+            throw new JasyptPluginException(e);
+        }
+    }
 
-	public static String decrypt(final String value, final String password, final String algorithm) throws JasyptPluginException {
-		try {
-			return stringEncryptor(password, algorithm).decrypt(value);
-		} catch (EncryptionOperationNotPossibleException e) {
-			throw new JasyptPluginException(e);
-		}
-	}
-
-	private static StringEncryptor stringEncryptor(final String password, final String algorithm) {
-		SimpleStringPBEConfig config = new SimpleStringPBEConfig();
-		config.setPassword(password);
-		StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
-		encryptor.setAlgorithm(algorithm);
-		encryptor.setIvGenerator(new RandomIvGenerator());
-		encryptor.setConfig(config);
-		return encryptor;
-	}
+    private static StringEncryptor stringEncryptor(final String password, final String algorithm) {
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setPassword(password);
+        StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+        encryptor.setAlgorithm(algorithm);
+        encryptor.setIvGenerator(new RandomIvGenerator());
+        encryptor.setConfig(config);
+        return encryptor;
+    }
 }
